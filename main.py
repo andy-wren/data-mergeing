636bfc9c14054b3a5a92169303aa9919c2ad0332
DoDo <119377201+andy-wren@users.noreply.github.com>
2023-11-30 17:10:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,64 @@
+import pandas
 import csv
 
-dataset_1 = []
-dataset_2 = []
+df = pandas.read_csv("brightest_stars.csv")
+df = df.dropna()
+df = df.drop_duplicates()
 
-with open("final.csv", "r") as f:
-    csvreader = csv.reader(f)
-    for row in csvreader: 
-        dataset_1.append(row)
+for i in range(len(df)):
+    try:
+        df["radius"][i] *= 0.102763
+        df["mass"][i] *= 0.000954588
+    except:
+        continue
 
-with open("archive_dataset_sorted.csv", "r") as f:
-    csvreader = csv.reader(f)
-    for row in csvreader: 
-        dataset_2.append(row)
+df.to_csv("brightest_stars_formatted.csv", index=False)
 
-data1 = dataset_1[0]
-planet_data_1 = dataset_1[1:]
+del df
 
-data2 = dataset_2[0]
-planet_data_2 = dataset_2[1:]
+df = pandas.read_csv("brown_dwarfs.csv")
+df = df.dropna()
+df = df.drop_duplicates()
 
-data = data1 + data2
-planet_data = []
-for index, data_row in enumerate(planet_data_1):
-    planet_data.append(planet_data_1[index] + planet_data_2[index])
+for i in range(len(df)):
+    try:
+        df["radius"][i] *= 0.102763
+        df["mass"][i] *= 0.000954588
+    except:
+        continue
 
-with open("merged_dataset.csv", "a+") as f:
-    csvwriter = csv.writer(f)
-    csvwriter.writerow(data)
-    csvwriter.writerows(planet_data)
+df.to_csv("brown_dwarfs_formatted.csv", index=False)
+
+del df
+
+data_1 = []
+data_2 = []
+
+with open("brightest_stars_formatted.csv") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        data_1.append(row)
+
+with open("brown_dwarfs_formatted.csv") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        data_2.append(row)
+
+headers_1 = data_1[0]
+star_data_1 = data_1[1:]
+
+headers_2 = data_2[0]
+star_data_2 = data_2[1:]
+
+headers = headers_1 + headers_2
+merged_data = []
+
+for index, data_row in enumerate(data_1):
+    if index == 0:
+        continue
+    merged_data.append(data_1[index] + data_2[index])
+
+with open("final_merged_data.csv", "a+") as file:
+    writer = csv.writer(file)
+    writer.writerow(headers)
+    writer.writerows(merged_data)
